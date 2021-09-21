fb9871429856781dfae4786462803b16ce4bda36
DmitrySinkov <dmitry.sinkov@kaseya.net>
2021-09-21 09:50:47 +0300

    add task-5 related changes:
     - update api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4vk79p9qn0.execute-api.eu-west-1.amazonaws.com/dev/',
   bff: 'https://svy7wl2ang.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
